ac67e5a6a1d79872e6994f5a8bb9cae5eb16cb98
Anthony Li <anthonyrli1994@gmail.com>
2023-02-02 23:55:30 -0500

    test

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname,'public')));
 
-//let notes = require(`${path.join(__dirname,'public')}/db/db.json`);
+let notes = require(`${path.join(__dirname,'public')}/db/db.json`);
 
 // GET route
 app.get('/api/notes', (req, res) => {
